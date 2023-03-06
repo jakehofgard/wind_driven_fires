683bcb374f548c3a483db6ddde843720657ebace
Aditi T <31555219+ololiti@users.noreply.github.com>
2023-03-06 03:39:50 -0800

    this is badly written but it should work? will improve later

--- src/observations.jl
+++ src/observations.jl
@@ -1,43 +1,34 @@
 # Observations
 # observation(pomdp, a, sp)
 function POMDPs.observation(pomdp::FireWorld, a::Array{Int64,1}, sp::FireState)
+    fn = pomdp.bprob_fn
+    fp = pomdp.bprob_fp
+
     neighbors = FireObs[]
     probabilities = Array{Float64,1}(undef,0)
-    
-    burning = sp.burning
-    fuels = sp.fuels
-    
-    P_xy = fire_spread(pomdp, sp)
-    s_new = update_burn_probs(sp, P_xy)
-    burn_probs_new = s_new.burn_probs
-    
-    # idea is
-    # update burning in observations
-    # only change the cells that were not acted upon
-    # because if action was applied, fp and fn rates are 0 (eyes on fire)
-    # otherwise, some delay happens and observation may not be the same
 
-    # update the burn_probs map for actions taken to be 1.0 (so it won't be sensitive to threshold we pick)
-    burn_probs_for_obs = deepcopy(burn_probs_new)
-    for a_i in a
-        burn_probs_for_obs[a_i] = 1.0
-    end
-
-    thresholds = collect(12:19)/20
-    individual_prob = 1/length(thresholds)
-    burns = []
-    for threshold_k in thresholds
-        burning_k = burn_probs_for_obs .> threshold_k
-        # need to check if already in
-        if burning_k in burns
-            b_idx = findall(x->x==burning_k, burns)
-            prob = probabilities[b_idx[1]]
-            probabilities[b_idx[1]] = prob + individual_prob
-        else
-            push!(burns, burning_k)
-            push!(probabilities, individual_prob)
-            push!(neighbors, FireObs(burning_k))
+    NUM_SAMPLES = 10
+    prob = 1/NUM_SAMPLES
+    for i in 1:NUM_SAMPLES
+        burn_obs = deepcopy(sp.burning)
+        for j in 1:length(burn_obs)
+            if sp.burning[j] == 1
+                if rand() < fn
+                    burn_obs[j] = 0
+                end
+            else
+                if rand() < fp
+                    burn_obs[j] = 1
+                end
+            end
         end
+        for ai in a
+            burn_obs[ai] = sp.burning[ai]
+        end
+
+        push!(neighbors, burn_obs)
+        push!(probabilities, prob)
     end
+
     return SparseCat(neighbors, probabilities)
 end
