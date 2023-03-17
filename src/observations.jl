f6db83aeae3eca64bf49941d20b10ffd024faf91
Aditi T <31555219+ololiti@users.noreply.github.com>
2023-03-16 20:24:45 -0700

    make observations an implicit distribution

--- src/observations.jl
+++ src/observations.jl
@@ -4,20 +4,15 @@ function POMDPs.observation(pomdp::FireWorld, a::Array{Int64,1}, sp::FireState)
     fn = pomdp.bprob_fn
     fp = pomdp.bprob_fp
 
-    neighbors = FireObs[]
-    probabilities = Array{Float64,1}(undef,0)
-
-    NUM_SAMPLES = 10
-    prob = 1 / NUM_SAMPLES
-    for i in 1:NUM_SAMPLES
+    ImplicitDistribution(sp, a) do sp, a, rng
         burn_obs = deepcopy(sp.burning)
         for j in 1:length(burn_obs)
             if sp.burning[j] == 1
-                if rand() < fn
+                if rand(rng, 1:fn) == 1
                     burn_obs[j] = 0
                 end
             else
-                if rand() < fp
+                if rand(rng, 1:fp) == 1
                     burn_obs[j] = 1
                 end
             end
@@ -25,10 +20,5 @@ function POMDPs.observation(pomdp::FireWorld, a::Array{Int64,1}, sp::FireState)
         for ai in a
             burn_obs[ai] = sp.burning[ai]
         end
-
-        push!(neighbors, FireObs(burn_obs))
-        push!(probabilities, prob)
     end
-
-    return SparseCat(neighbors, probabilities)
 end
