4879b8de2ba946729ac241e54e5f3682768f2d4f
Aditi T <31555219+ololiti@users.noreply.github.com>
2023-03-20 14:26:29 -0700

    finalized ardepsot, basicpomcp

--- src/ardespot.jl
+++ src/ardespot.jl
@@ -6,6 +6,8 @@ using POMDPPolicies
 using POMDPSimulators
 using ParticleFilters
 using Random
+using DataFrames
+using CSV
 using LinearAlgebra
 using StatsBase
 using Distances
@@ -15,78 +17,85 @@ using .FirePOMDP
 include("updater.jl")
 include("observations.jl")
 
-GRID_SIZE = 4
-MAX_ACT = ceil(0.25 * GRID_SIZE)
+GRID_SIZES = [4]
+results = DataFrame([[],[],[],[],[]], ["size", "n_default", "n_online", "reward_default", "reward_online"])
+NUM_ITERS = 50
 
-pomdp = FireWorld(grid_size = GRID_SIZE, tprob=1.0)
+pomdp = FireWorld(grid_size=4, tprob=0.8)
 
-up = HistoryUpdater();
+for iter in 1:NUM_ITERS
+    for grid_size in GRID_SIZES
+        println("Testing grid size ", grid_size)
+        max_act = ceil(0.25 * grid_size)
 
-s0 = rand(initialstate(pomdp))
-b0 = initialize_belief(up, initialstate(pomdp))
+        global pomdp = FireWorld(grid_size=grid_size, tprob=0.8)
 
-# In line with DESPOT paper, upper bound is R_max/(1 - γ), lower bound is determined by random policy
-rng = MersenneTwister()
-random_policy = RandomPolicy(rng, pomdp, up)
-lower = DefaultPolicyLB(random_policy)
-upper = 0.0
+        up = HistoryUpdater();
 
-COSTS = sortperm(pomdp.costs)
+        s0 = rand(initialstate(pomdp))
+        b0 = initialize_belief(up, initialstate(pomdp))
 
-function default_act(belief)
-    obs = rand(belief).burning
-    a = Int64[]
-    total = 0
-    for i in 1:length(COSTS)
-        if obs[COSTS[i]] == 1
-            push!(a, i)
-            total += 1
-            if total == MAX_ACT
-                break
+        # In line with DESPOT paper, upper bound is R_max/(1 - γ), lower bound is determined by random policy
+        lower = -250.0
+        upper = 0.0
+
+        costs = sortperm(pomdp.costs)
+
+        function default_act(belief)
+            obs = rand(belief).burning
+            a = Int64[]
+            total = 0
+            for i in 1:length(costs)
+                if obs[costs[i]] == 1
+                    push!(a, i)
+                    total += 1
+                    if total == max_act
+                        break
+                    end
+                end
             end
+            while total < max_act
+                push!(a, 1)
+                total += 1
+            end
+            return a
         end
+
+        function default_for_solver(belief, ex)
+        #     @warn ex
+            return default_act(belief)
+        end
+
+        # Tune hyperparameters here!
+        solver = DESPOTSolver(
+            bounds=IndependentBounds(lower, upper),
+            max_trials=10, # tune
+            default_action=default_for_solver
+        )
+
+        planner = solve(solver, pomdp);
+        print("solved")
+
+        # The following experiment tests a randomly generated policy against POMCPOW
+
+        rng = MersenneTwister()
+        default_policy = FunctionPolicy(default_act)
+
+        default = Sim(pomdp, default_policy, up, b0, s0)
+        online = Sim(pomdp, planner, up, b0, s0)
+
+        sim_default = simulate(default)
+        reward_default = discounted_reward(sim_default)
+        steps_default = n_steps(sim_default)
+        print("simulated default, reward is $reward_default")
+
+        sim_online = simulate(online)
+        reward_online = discounted_reward(sim_online)
+        steps_online = n_steps(sim_online)
+        print("simulated online, reward is $reward_online")
+
+        push!(results, [grid_size, steps_default, steps_online, reward_default, reward_online])
+        CSV.write("../experiments/ardespot_3.csv", results)
     end
-    while total < MAX_ACT
-        push!(a, 1)
-        total += 1
-    end
-    return a
-end
-
-function default_for_solver(belief, ex)
-#     @warn ex
-    return default_act(belief)
-end
-
-# Tune hyperparameters here!
-solver = DESPOTSolver(
-    bounds=IndependentBounds(lower, upper),
-    max_trials=10, # tune
-    default_action=default_for_solver
-)
-
-planner = solve(solver, pomdp);
-
-# Stepthrough entire simulation
-# using Profile
-
-# for (s,a,r,sp,o) in stepthrough(pomdp, planner, up, b0, s0, "s, a, r, sp, o")
-#     # println("in state $s")
-#     println("took action $a")
-#     println("received reward $r")
-#     # println("received observation $o and reward $r")
-# end
-
-# The following experiment tests a randomly generated policy against POMCPOW
-
-rng = MersenneTwister()
-default_policy = FunctionPolicy(default_act)
-
-default = Sim(pomdp, default_policy, up, b0, s0)
-ardespot = Sim(pomdp, planner, up, b0, s0)
-
-run([default, ardespot]) do sim, hist
-    reward = discounted_reward(hist)
-    print("got discounted reward of $reward")
-    return (n_steps=n_steps(hist), reward=discounted_reward(hist))
+    println("Finished iter ", iter)
 end
--- src/basicpomcp.jl
+++ src/basicpomcp.jl
@@ -14,6 +14,8 @@ using POMDPPolicies
 using POMDPSimulators
 using ParticleFilters
 using Random
+using DataFrames
+using CSV
 using LinearAlgebra
 using StatsBase
 using Distances
@@ -23,71 +25,81 @@ using .FirePOMDP
 include("updater.jl")
 include("observations.jl")
 
-GRID_SIZES = [4, 6, 8, 10, 12]
-results = DataFrame([[],[],[]], ["size", "n_steps", "reward"])
 
-grid_size = 4
-max_act = ceil(0.25 * grid_size)
-pomdp = FireWorld(grid_size = grid_size, tprob=0.8)
-COSTS = sortperm(pomdp.costs)
+GRID_SIZES = [4, 6]
+results = DataFrame([[],[],[],[],[]], ["size", "n_default", "n_online", "reward_default", "reward_online"])
+NUM_ITERS = 10
 
-function default_act(belief)
-    obs = rand(belief).burning
-    a = Int64[]
-    total = 0
-    for i in 1:length(COSTS)
-        if obs[COSTS[i]] == 1
-            push!(a, i)
-            total += 1
-            if total == max_act
-                break
+pomdp = FireWorld(grid_size=4, tprob=0.8)
+
+for iter in 1:NUM_ITERS
+    for grid_size in GRID_SIZES
+        println("Testing grid size ", grid_size)
+        max_act = ceil(0.25 * grid_size)
+
+        global pomdp = FireWorld(grid_size = grid_size, tprob=0.8)
+        costs = sortperm(pomdp.costs)
+
+        function default_act(belief)
+            obs = rand(belief).burning
+            a = Int64[]
+            total = 0
+            for i in 1:length(costs)
+                if obs[costs[i]] == 1
+                    push!(a, i)
+                    total += 1
+                    if total == max_act
+                        break
+                    end
+                end
             end
+            while total < max_act
+                push!(a, 1)
+                total += 1
+            end
+            return a
         end
+
+        function default_for_solver(belief, ex)
+            # @warn ex
+            return default_act(belief)
+        end
+
+        # Tune hyperparameters here!
+        solver = POMCPSolver(
+            rng=MersenneTwister(264),
+            default_action = default_for_solver,
+            max_depth = 20, # Tune
+            tree_queries = 2000, # Tune
+            c = 10.0 # Tune
+        )
+
+        planner = solve(solver, pomdp);
+        up = HistoryUpdater();
+
+        s0 = rand(initialstate(pomdp))
+        b0 = initialize_belief(up, initialstate(pomdp))
+
+        # The following experiment tests a randomly generated policy against POMCPOW
+
+        rng = MersenneTwister()
+        default_policy = FunctionPolicy(default_act)
+
+        default = Sim(pomdp, default_policy, up, b0, s0)
+        online = Sim(pomdp, planner, up, b0, s0)
+
+        sim_default = simulate(default)
+        reward_default = discounted_reward(sim_default)
+        steps_default = n_steps(sim_default)
+
+        sim_online = simulate(online)
+        reward_online = discounted_reward(sim_online)
+        steps_online = n_steps(sim_online)
+
+        push!(results, [grid_size, steps_default, steps_online, reward_default, reward_online])
     end
-    while total < max_act
-        push!(a, 1)
-        total += 1
-    end
-    return a
+    println("Finished iter ", iter)
+    CSV.write("../experiments/basicpomcp4.csv", results)
 end
 
-function default_for_solver(belief, ex)
-    # @warn ex
-    return default_act(belief)
-end
 
-# Tune hyperparameters here!
-solver = POMCPSolver(
-    rng=MersenneTwister(264),
-    default_action = default_for_solver,
-    max_depth = 20, # Tune
-    tree_queries = 1000, # Tune
-    c = 5.0 # Tune
-)
-
-planner = solve(solver, pomdp);
-up = HistoryUpdater();
-
-s0 = rand(initialstate(pomdp))
-b0 = initialize_belief(up, initialstate(pomdp))
-
-# The following experiment tests a randomly generated policy against POMCPOW
-
-rng = MersenneTwister()
-default_policy = FunctionPolicy(default_act)
-
-default = Sim(pomdp, default_policy, up, b0, s0)
-pomcp = Sim(pomdp, planner, up, b0, s0)
-
-reward_default = discounted_reward(simulate(default))
-reward_pomcp = discounted_reward(simulate(pomcp))
-
-
-run([default, pomcp]) do sim, hist
-    reward = discounted_reward(hist)
-    println("got discounted reward of $reward")
-    push!(results, [grid_size, n_steps(hist), discounted_reward(hist)])
-    return (n_steps=n_steps(hist), reward=discounted_reward(hist))
-end
-
-results
