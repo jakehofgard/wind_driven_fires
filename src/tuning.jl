98287258a276fade3666fb0f5cffe2befd0283b8
jakehofgard <53575125+jakehofgard@users.noreply.github.com>
2023-03-20 12:20:54 -0700

    Finalize repo

--- src/tuning.jl
+++ src/tuning.jl
@@ -21,16 +21,20 @@ end
 
 # Plot all hyperparameter tuning
 
-param_tuning_first = plot(c_vals, reward_diffs_4, ma=0.7, label="\$n = 4\$")
-plot!(c_vals, reward_diffs_6, ma=0.7, label="\$n = 6\$")
-plot!(c_vals, reward_diffs_8, ma=0.7, label="\$n = 8\$")
+param_tuning_first = plot(c_vals, reward_diffs_4, color=:green, xscale=:log10, ma=0.7, label="")
+scatter!(c_vals, reward_diffs_4, ma=0.7, color=:green, label="\$n = 4\$")
+plot!(c_vals, reward_diffs_6, ma=0.7, color=:blue, label="")
+scatter!(c_vals, reward_diffs_6, ma=0.7, label="\$n = 6\$", color=:blue)
+plot!(c_vals, reward_diffs_8, ma=0.7, color=:red, label="")
+scatter!(c_vals, reward_diffs_8, ma=0.7, label="\$n = 8\$", color=:red)
 title!("UCB1 Constant Hyperparameter Tuning for \$n = 4,6,8\$")
 xlabel!("UCB1 Parameter \$c\$")
 ylabel!("Average Advantage \$δ\$")
 
 savefig(param_tuning_first, "src/figures/mcts_hyperparam_tuning_small.png")
 
-param_tuning_10 = plot(c_vals, reward_diffs_10, ma=0.7, label="\$n = 10\$")
+param_tuning_10 = plot(c_vals, reward_diffs_10, ma=0.7, xscale=:log10, color=:orange, label="")
+scatter!(c_vals, reward_diffs_10, ma=0.7, color=:orange, label="\$n=10\$")
 title!("UCB1 Constant Hyperparameter Tuning for \$n = 10\$")
 xlabel!("UCB1 Parameter \$c\$")
 ylabel!("Average Advantage \$δ\$")
@@ -40,3 +44,13 @@ savefig(param_tuning_10, "src/figures/mcts_hyperparam_tuning_n=10.png")
 # data = CSV.read("src/experiments/pomcpow_c=0.2_n=8.csv", DataFrame)
 # grouped = combine(groupby(data, :size), Not(:size) .=> mean)
 # grouped[!, "reward_online_mean"] - grouped[!, "reward_default_mean"]
+
+data = CSV.read("src/experiments/pomcpow_c=0.2_n=8.csv", DataFrame)
+grouped = combine(groupby(data, :size), Not(:size) .=> mean)
+advantage = data[!, "reward_online"] - data[!, "reward_default"]
+hist_8 = histogram(advantage, bins=range(-50, 250, 12), ma=0.2, color=:lightcoral, label="")
+title!("POMCPOW Advantage Over Default Policy for \$n=8\$")
+xlabel!("Advantage \$δ\$")
+ylabel!("Count")
+
+savefig(hist_8, "src/figures/advantage_resids_n=8.png")
